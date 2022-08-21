5ee3306a0c262733054529198266384b769e68b5
SantiagoBot321 <boteroramirezsantiago@gmail.com>
2022-08-21 00:14:16 -0500

    Hola soy santiago botero

--- Sprint1_Ciclo3/src/main/java/org/example/Main.java
+++ Sprint1_Ciclo3/src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Soy Santiago Trujillo y este es mi commit :)");
+        System.out.println("Soy Santiago Botero y este es mi commit");
     }
 }
