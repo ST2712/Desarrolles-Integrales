f5a85766e16965e5603adb72b4cf7aff01354e78
david1022371019 <111076846+david1022371019@users.noreply.github.com>
2022-08-21 21:59:33 -0500

    Add files via upload
    Soy CRistian Moncaleno

---
+++ Desarrolles-Integrales/Sprint1_Ciclo3/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Soy Santiago Trujillo y este es mi commit :)");
+        System.out.println("Soy Cristian Moncaleno");
+    }
+}
